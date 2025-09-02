90394b9a64fd22b7b841b2b134460c69a443b313
Om Khairnar <om.khairnar175@gmail.com>
2025-09-02 11:09:28 +0530

    changed the credentials here

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:44302/api',
+  apiUrl: 'https://gammon-pts.vercel.app/api',
   msalConfig: {
     clientId: 'ce82a460-db03-4e9d-a0c4-2a062dd9c5ed',
     authority: 'https://login.microsoftonline.com/668c53aa-445f-4661-a18d-d9e542601f44',
-    redirectUri: 'http://localhost:4200/auth/callback',
+    redirectUri: 'https://gammon-pts.vercel.app/auth/callback',
   },
 };
