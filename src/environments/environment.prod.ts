db44f0c42a6a50ec9f152d93b173cc2a65743a28
Om Khairnar <om.khairnar175@gmail.com>
2025-09-24 12:44:50 +0530

    added some issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://app-gammon-pts-dev-backend.azurewebsites.net/api',
+  apiUrl: '/api',
   msalConfig: {
     clientId: 'ce82a460-db03-4e9d-a0c4-2a062dd9c5ed',
     authority: 'https://login.microsoftonline.com/668c53aa-445f-4661-a18d-d9e542601f44',
