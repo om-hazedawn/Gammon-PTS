9fa192b9b1994dbf04021f268ff1dbd9d0b627bd
Om Khairnar <om.khairnar175@gmail.com>
2025-09-24 13:18:38 +0530

    added older env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
   msalConfig: {
     clientId: 'ce82a460-db03-4e9d-a0c4-2a062dd9c5ed',
     authority: 'https://login.microsoftonline.com/668c53aa-445f-4661-a18d-d9e542601f44',
-    redirectUri: 'http://localhost:4200/auth/callback',
+    redirectUri: 'https://gammon-pts.vercel.app/auth/callback',
   },
 };
